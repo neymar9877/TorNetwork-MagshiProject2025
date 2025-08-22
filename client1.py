6c770550748b4c14200d29fd2754ebf35103283d
unknown <lozaalon2008@gmail.com>
2025-08-22 15:54:33 +0300

    connectin between two clients

--- client1.py
+++ client1.py
@@ -1,1 +1,31 @@
-print("Im the first client")
+#print("Im the first client")
+
+import socket
+PORT = 1535
+IP = '127.0.0.1'
+
+def main():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+
+    # Connecting to the other client
+    client2_address = (IP, PORT)
+    sock.connect(client2_address)
+
+    #SEND MESSAGE TO CLIENT2
+    msg = "first client says hello!!!"
+    sock.sendall(msg.encode())
+    #GET MESSAGE  FROM CLIENT2 
+    client2_msg = sock.recv(1024)
+    client2_msg = client2_msg.decode()
+    print(client2_msg)
+
+
+    sock.close()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
--- client2.py
+++ client2.py
@@ -1,7 +1,37 @@
-print("Im the second client")
+#print("Im the second client")
+
+import socket
+PORT = 1535
+IP = '127.0.0.1'
 
 def main():
-    pass
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+
+    # Connecting to the other client
+    address = ('', PORT)
+    sock.bind(address)
+
+
+    # Listen for incoming connections
+    sock.listen(1)
+
+    # Create a new conversation socket
+    client_soc, client_address = sock.accept()
+
+    # GET MESSAGE FROM CLIENT1
+    client_msg = client_soc.recv(1024)
+    client_msg = client_msg.decode()
+    print(client_msg)
+
+    #SEND MESSAGE TO CLIENT1
+    msg = "second client says hello!!!"
+    client_soc.sendall(msg.encode())
+
+
+    sock.close()
+
+
 
 
 
